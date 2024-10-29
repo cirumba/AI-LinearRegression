31349b069bf6d2e20c4f83614db2292ee764145e
Guilherme Cirumbolo <gcirumbolo@gmail.com>
2024-10-28 21:57:08 -0300

    hotfix

--- alegrete.py
+++ alegrete.py
@@ -2,27 +2,13 @@ import numpy as np
 
 
 def compute_mse(b, w, data):
-<<<<<<< HEAD
-    """
-    Calcula o erro quadratico medio
-    :param b: float - bias (intercepto da reta)
-    :param w: float - peso (inclinacao da reta)
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :return: float - o erro quadratico medio
-    """
+
     total_error = np.mean((data[:, 1] - (w * data[:, 0] + b)) ** 2)
     return total_error
 
 
 def step_gradient(b, w, data, alpha):
-    """
-    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de b e w.
-    :param b: float - bias (intercepto da reta)
-    :param w: float - peso (inclinacao da reta)
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :return: float,float - os novos valores de b e w, respectivamente
-    """
+
     N = len(data)
     b_gradient = -2 / N * np.sum(data[:, 1] - (w * data[:, 0] + b))
     w_gradient = -2 / N * np.sum(data[:, 0] * (data[:, 1] - (w * data[:, 0] + b)))
@@ -34,20 +20,7 @@ def step_gradient(b, w, data, alpha):
 
 
 def fit(data, b, w, alpha, num_iterations):
-    """
-    Para cada época/iteração, executa uma atualização por descida de
-    gradiente e registra os valores atualizados de b e w.
-    Ao final, retorna duas listas, uma com os b e outra com os w
-    obtidos ao longo da execução (o último valor das listas deve
-    corresponder à última época/iteração).
-
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param b: float - bias (intercepto da reta)
-    :param w: float - peso (inclinacao da reta)
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
-    :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
-    """
+   
     b_values = [b]
     w_values = [w]
 
